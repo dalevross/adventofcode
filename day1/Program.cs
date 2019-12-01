cf34418a92e7a910bb959d6873ca437dbf61994d
Dale V Ross <kingdale20@gmail.com>
2019-12-01 04:09:56 -0500

    Completed AdventOfCode Day1

---
+++ day1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sum = 0;
+            int sumfuel = 0;
+            using (StreamReader sr = new StreamReader("input.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int mass = int.Parse(line);
+                    sum += calculateFuel(mass);
+                    sumfuel += calculatewithFuel(mass);
+                }
+
+            }
+            Console.WriteLine(sum);
+            Console.WriteLine(calculatewithFuel(14));
+            Console.WriteLine(sumfuel);
+
+        }
+
+        private static int calculatewithFuel(int mass)
+        {
+            if (calculateFuel(mass) <= 0)
+                return 0;
+            else
+            {
+                int fuel = calculateFuel(mass);
+                return fuel + calculatewithFuel(fuel);
+            }
+        }
+
+        static int calculateFuel(int mass)
+        {
+            return (int)Math.Floor(mass / 3.0) - 2;
+        }
+    }
+}
