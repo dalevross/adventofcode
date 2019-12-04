e7440b9fee9fe6bea19039f6e9d46a7241fd293d
Dale V Ross <dalevross@gmail.com>
2019-12-03 21:46:46 -0500

    Completed day3 - Updated Readme with links to solution files.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Advent Of Code 2019
 
-[Day 1](https://adventofcode.com/2019/day/1) &#127775;&#127775;
+[Day 1](https://adventofcode.com/2019/day/1) &#127775;&#127775; [Solution](day1/Program.cs)
 
-[Day 2](https://adventofcode.com/2019/day/2) &#127775;&#127775;
+[Day 2](https://adventofcode.com/2019/day/2) &#127775;&#127775; [Solution](day2/Program.cs)
 
-[Day 3](https://adventofcode.com/2019/day/3) &#127775;
+[Day 3](https://adventofcode.com/2019/day/3) &#127775;&#127775; [Solution](day3/Program.cs)
--- day3/Program.cs
+++ day3/Program.cs
@@ -22,21 +22,24 @@ namespace day3
 
 
             }
-            Console.WriteLine(getDistance(new int[] { 0, 0 }, wire1, wire2));
+            Console.WriteLine(getDistances(new int[] { 0, 0 }, wire1, wire2));
 
         }
 
-        static int getDistance(int[] startpoint, string[] wire1, string[] wire2)
+        static Tuple<int,int> getDistances(int[] startpoint, string[] wire1, string[] wire2)
         {
-            int minDistance = Int32.MaxValue;
-            Dictionary<Tuple<int,int>,bool> referencePoints = new Dictionary<Tuple<int,int>, bool>();
+            int minManHattenDistance = Int32.MaxValue;
+            int minTotalDistance = Int32.MaxValue;
+            Dictionary<Tuple<int,int>,List<int>> referencePoints = new Dictionary<Tuple<int,int>, List<int>>();
             List<int[]> intersect = new List<int[]>();
             List<int[]> points1 = getPoints((int[])startpoint.Clone(), wire1);
             List<int[]> points2 = getPoints((int[])startpoint.Clone(), wire2);
             foreach(int[] point1 in points1)
             {
-                if(!referencePoints.ContainsKey(new Tuple<int, int>(point1[0],point1[1])))
-                    referencePoints.Add(new Tuple<int, int>(point1[0],point1[1]),true);
+                 if(referencePoints.ContainsKey(new Tuple<int, int>(point1[0],point1[1])))
+                    referencePoints[new Tuple<int, int>(point1[0],point1[1])].Add(point1[2]);
+                 else
+                    referencePoints.Add(new Tuple<int, int>(point1[0],point1[1]),new List<int>{point1[2]});
                 
             }
 
@@ -44,20 +47,26 @@ namespace day3
             {
                 if(referencePoints.ContainsKey(new Tuple<int, int>(point2[0],point2[1])))
                 {
-                    intersect.Add(new int[]{point2[0],point2[1]});
+                    foreach(int distanceforwire1 in referencePoints[new Tuple<int, int>(point2[0],point2[1])])
+                    {
+                        intersect.Add(new int[]{point2[0],point2[1],distanceforwire1 + point2[2]});
+                    }
                 }
                     
             }
             //int minDistance = Int32.MaxValue;
             foreach(int[] point in intersect)
             {
+                if(point[2] < minTotalDistance)
+                    minTotalDistance = point[2];
+
                 int manhattenDistance = Math.Abs(point[1] - startpoint[1]) + Math.Abs(point[0] - startpoint[0]);
-                if(manhattenDistance < minDistance)
+                if(manhattenDistance < minManHattenDistance)
                 {
-                    minDistance = manhattenDistance;
+                    minManHattenDistance = manhattenDistance;
                 }
             }
-            return minDistance;
+            return new Tuple<int, int>(minManHattenDistance,minTotalDistance);
         }
 
         
@@ -65,6 +74,7 @@ namespace day3
         {
             int[] currentPoint = (int[])startpoint.Clone();
             List<int[]> points = new List<int[]>();
+            int totalCount = 0;
             foreach (string movement in wire)
             {
                 char dir = movement[0];
@@ -75,16 +85,16 @@ namespace day3
                         foreach (int i in Enumerable.Range(currentPoint[0] + 1, count))
                         {
                             if(!(new int[] { i, currentPoint[1] }.SequenceEqual(new int[]{0,0})))
-                                points.Add(new int[] { i, currentPoint[1] });
+                                points.Add(new int[] { i, currentPoint[1],++totalCount });
 
                         }
                         currentPoint[0] += count;
                         break;
                     case 'L':
-                        foreach (int i in Enumerable.Range(currentPoint[0] - count, count - 1))
+                        foreach (int i in Enumerable.Range(currentPoint[0] - count, count).Reverse())
                         {
                             if(!(new int[] { i, currentPoint[1] }.SequenceEqual(new int[]{0,0})))
-                                points.Add(new int[] { i, currentPoint[1] });
+                                points.Add(new int[] { i, currentPoint[1] ,++totalCount});
 
                         }
                         currentPoint[0] -= count;
@@ -93,16 +103,16 @@ namespace day3
                         foreach (int i in Enumerable.Range(currentPoint[1] + 1, count))
                         {
                             if(!(new int[] { currentPoint[0], i }.SequenceEqual(new int[]{0,0})))
-                                points.Add(new int[] { currentPoint[0], i });
+                                points.Add(new int[] { currentPoint[0], i ,++totalCount});
 
                         }
                         currentPoint[1] += count;
                         break;
                     case 'D':
-                        foreach (int i in Enumerable.Range(currentPoint[1] - count, count - 1))
+                        foreach (int i in Enumerable.Range(currentPoint[1] - count, count).Reverse())
                         {
                             if(!(new int[] { currentPoint[0], i }.SequenceEqual(new int[]{0,0})))
-                                points.Add(new int[] { currentPoint[0], i });
+                                points.Add(new int[] { currentPoint[0], i,++totalCount });
 
                         }
                         currentPoint[1] -= count;
