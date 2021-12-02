8e3fdaa87f24b296bb736b98d1fb14be34455bce
Dale V Ross <dalevross@gmail.com>
2021-12-02 00:26:21 -0500

    Day 2

---
+++ 2021/day2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horizontalPos = 0;
+            
+            int verticalPos = 0;
+            using(StreamReader sr = new StreamReader("input.txt"))
+            {
+               
+                string instruction;
+                while((instruction = sr.ReadLine()) != null)
+                {
+                    string[] instructions = instruction.Split(' ');
+                    switch(instructions[0])
+                    {
+                        case "forward":
+                           horizontalPos += Int32.Parse(instructions[1]);
+                           break;
+                        case "down":
+                           verticalPos += Int32.Parse(instructions[1]);
+                           break;
+                           case "up":
+                        verticalPos -= Int32.Parse(instructions[1]);
+                           break;
+                        default:
+                            break;
+
+;                    }
+                }
+            }
+            Console.WriteLine("Result {0}",horizontalPos * verticalPos);
+
+            horizontalPos = 0;
+            
+            verticalPos = 0;
+            int aim = 0;
+            using(StreamReader sr = new StreamReader("input.txt"))
+            {
+               
+                string instruction;
+                while((instruction = sr.ReadLine()) != null)
+                {
+                    string[] instructions = instruction.Split(' ');
+                    switch(instructions[0])
+                    {
+                        case "forward":
+                            horizontalPos += Int32.Parse(instructions[1]);
+                            verticalPos += aim * Int32.Parse(instructions[1]);
+                            break;
+                        case "down":
+                            aim += Int32.Parse(instructions[1]);
+                            break;
+                            case "up":
+                            aim -= Int32.Parse(instructions[1]);
+                            break;
+                        default:
+                            break;
+
+;                    }
+                }
+            }
+
+             Console.WriteLine("Result {0}",horizontalPos * verticalPos);
+        }
+
+    
+    }
+
+}
--- README.md
+++ README.md
@@ -43,3 +43,5 @@
 # Advent Of Code 2021
 
 [Day 1](https://adventofcode.com/2021/day/1) &#127775;&#127775; [Solution](2021/day1/Program.cs)
+
+[Day 2](https://adventofcode.com/2021/day/2) &#127775;&#127775; [Solution](2021/day2/Program.cs)
