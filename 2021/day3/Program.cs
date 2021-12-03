d5e2ee03a978b1ab747f4f241940faefa5635095
Dale V Ross <dalevross@gmail.com>
2021-12-03 02:11:54 -0500

    Day3

---
+++ 2021/day3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader sr = new StreamReader("input.txt"))
+            {
+                string[] bits = sr.ReadToEnd().Split('\n');
+                bits = bits.Select(x => x.Trim()).ToArray<string>();
+                
+                String gammaRate = getGammaRate(bits);
+                String epsilonRate = getEpsilonRate(bits);
+                Console.WriteLine(Convert.ToInt32(gammaRate, 2) 
+                * Convert.ToInt32(epsilonRate, 2) );
+
+                String oxygenRating =  getOxygenGeneraterRating(bits);
+                String cO2ScrubberRating = getCO2ScrubberRating(bits);
+                  Console.WriteLine(Convert.ToInt32(oxygenRating, 2) 
+                * Convert.ToInt32(cO2ScrubberRating, 2) );
+
+
+                
+            }
+           
+        }
+
+
+        static string getGammaRate(string[] bits)
+        {
+            StringBuilder gammaRate = new StringBuilder();
+            for(int i = 0;i<bits[0].Length;i++)
+            {
+                    char gammaRateI = ( bits.Select(x=>x[i]).Count(c => c == '1') >= (0.5 * bits.Length))? '1' : '0';
+                    gammaRate.Append(gammaRateI);
+            }
+            return gammaRate.ToString();
+        }
+
+        static string getEpsilonRate(string[] bits)
+        {
+            StringBuilder epsilonRate = new StringBuilder();
+            for(int i = 0;i<bits[0].Length;i++)
+            {
+                    char epsilonRateI = ( bits.Select(x=>x[i]).Count(c => c == '0') <= (0.5 * bits.Length))? '0' : '1';
+                    epsilonRate.Append(epsilonRateI);
+            }
+            return epsilonRate.ToString();
+        }
+
+        static string getOxygenGeneraterRating(string[] bits)
+        {
+            int index = 0;
+            while(bits.Length > 1)
+            {
+                string gammaRate = getGammaRate(bits);
+                bits = bits.Where(bit => bit[index] == gammaRate[index]).ToArray<string>();
+                index++;
+            }
+            return bits[0];
+        }
+
+        static string getCO2ScrubberRating(string[] bits)
+        {
+            int index = 0;
+            while(bits.Length > 1)
+            {
+                string epsilonRate = getEpsilonRate(bits);
+                bits = bits.Where(bit => bit[index] == epsilonRate[index]).ToArray<string>();
+                index++;
+            }
+            return bits[0];
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -45,3 +45,5 @@
 [Day 1](https://adventofcode.com/2021/day/1) &#127775;&#127775; [Solution](2021/day1/Program.cs)
 
 [Day 2](https://adventofcode.com/2021/day/2) &#127775;&#127775; [Solution](2021/day2/Program.cs)
+
+[Day 3](https://adventofcode.com/2021/day/3) &#127775;&#127775; [Solution](2021/day3/Program.cs)
