4bd680c9ccca26ab222cdf330ac415ed7d687bfa
Dale V Ross <dalevross@gmail.com>
2021-12-06 10:23:33 -0500

    Day 6, Part 1

---
+++ 2021/day6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(StreamReader sr = new StreamReader("input.txt"))
+            {
+                int numberofGenerations = 80;
+                List<int> laternFishes = sr.ReadLine().Split(',').Select(x => Int32.Parse(x)).ToList();
+                for(int i = 0;i<numberofGenerations;i++)
+                {
+                    int numberOfZeroes = laternFishes.Count(x=>x==0);
+                    
+                    laternFishes.AddRange(Enumerable.Repeat(9,numberOfZeroes));
+                    laternFishes = laternFishes.Select((x,i) =>  (x == 0)? 6 : x -1).ToList();
+                }
+                Console.WriteLine(laternFishes.Count);
+            }
+           
+        }
+    }
+}
