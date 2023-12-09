6aa9f5066cba30cdd5d18f7f1c03c6bac1d0a122
dalevross <dalevross@gmail.com>
2023-12-09 09:09:26 -0500

    Completed Parts 1 & 2

---
+++ 2023/day9/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(PartOne());
+Console.WriteLine(PartTwo());
+
+static long PartOne()
+{
+
+    long sum = 0;
+
+    using(StreamReader sr = new StreamReader("input.txt"))
+    {
+        string? line;
+        while((line = sr.ReadLine()) != null)
+        {
+            List<List<long>> sequences = new List<List<long>>();
+            List<long> nextSequence = line.Split(' ').Select(x => Int64.Parse(x)).ToList();
+            sequences.Add(nextSequence);
+            while(!nextSequence.All( x => x == 0))
+            {
+               nextSequence = nextSequence
+               .Where( (el, index) => index > 0)
+               .Select((newEl,index) => nextSequence[index + 1] - nextSequence[index]).ToList();
+                sequences.Add(nextSequence);
+
+            }
+            for(int i = sequences.Count -1; i > 0; i--)
+            {
+                sequences[i-1].Add(sequences[i-1].Last() + sequences[i].Last());
+            }
+
+            sum += sequences[0].Last();
+
+
+
+        }
+
+        
+      
+    }
+    return sum;
+}
+
+
+static long PartTwo()
+{
+
+    long sum = 0;
+
+    using(StreamReader sr = new StreamReader("input.txt"))
+    {
+        string? line;
+        while((line = sr.ReadLine()) != null)
+        {
+            List<List<long>> sequences = new List<List<long>>();
+            List<long> nextSequence = line.Split(' ').Select(x => Int64.Parse(x)).ToList();
+            sequences.Add(nextSequence);
+            while(!nextSequence.All( x => x == 0))
+            {
+               nextSequence = nextSequence
+               .Where( (el, index) => index > 0)
+               .Select((newEl,index) => nextSequence[index + 1] - nextSequence[index]).ToList();
+                sequences.Add(nextSequence);
+
+            }
+            for(int i = sequences.Count -1; i > 0; i--)
+            {
+                sequences[i-1].Insert(0,(sequences[i-1].First()  - sequences[i].First()));
+            }
+
+            sum += sequences[0].First();
+
+
+
+        }
+
+        
+      
+    }
+    return sum;
+}
--- README.md
+++ README.md
@@ -71,3 +71,5 @@
 [Day 7](https://adventofcode.com/2023/day/7) &#127775;&#127775; [Solution](2023/day7/Program.cs)
 
 [Day 8](https://adventofcode.com/2023/day/8) &#127775;&#127775; [Solution](2023/day8/Program.cs)
+
+[Day 9](https://adventofcode.com/2023/day/9) &#127775;&#127775; [Solution](2023/day9/Program.cs)
